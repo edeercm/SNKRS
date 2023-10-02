363ab006a3f948bcd7df681147159aff003de30b
Eder <132967872+edeercm@users.noreply.github.com>
2023-10-02 16:00:26 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@ import ShopContext from './components/shopcontext'
 import Layout from './components/Layout'
 import Home from './pages/Home/home'
 import Shop from './pages/Shop/shop'
-// import Blog from './pages/blog'
 import About from './pages/About/about'
 import Contact from './pages/Contact/contact'
-import Wishlist from './pages/wishlist'
 import Login from './pages/Login/login'
 import ResetPassword from './pages/ResetPassword/resetpassword'
 import CreateAccount from './pages/CreateAccount/createaccount'
@@ -24,10 +22,8 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='shop' element={<Shop />} />
-          {/* <Route path='blog' element={<Blog />} /> */}
           <Route path='about' element={<About />} />
           <Route path='contact' element={<Contact />} />
-          <Route path='wishlist' element={<Wishlist />} />
           <Route path='login' element={<Login />} />
           <Route path='resetpassword' element={<ResetPassword />} />
           <Route path='createaccount' element={<CreateAccount />} />
