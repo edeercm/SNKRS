eeaf696ac8a17cc3090eea7a819beece0fec1e17
Eder <132967872+edeercm@users.noreply.github.com>
2024-01-29 14:31:13 -0600

    Update

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header/header'
-import Footer from '../components/Footer/footer'
+import Footer from './Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
