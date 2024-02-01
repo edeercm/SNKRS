e5044ef5e33ab0a50526ce8a710a0d06715a77ad
Eder <132967872+edeercm@users.noreply.github.com>
2024-02-01 13:03:54 -0600

    Update

--- src/pages/Home/HomeSecOne.jsx
+++ src/pages/Home/HomeSecOne.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Banner = styled.div`
+  display: block;
   width: 100%;
   height: 70vh;
-  background: url(src/assets/images/banner/homebanner.jpg);
+  background: url(/src/assets/images/banner/homebanner.jpg);
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
